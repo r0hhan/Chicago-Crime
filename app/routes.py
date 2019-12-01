537bf778989dd7118d6b76e8b0465125cdbf92b2
Rohit Choudhari <itsgood37@ymail.com>
2019-12-01 01:24:32 -0500

    Templating

--- app/routes.py
+++ app/routes.py
@@ -8,9 +8,22 @@ import connect
 def index():
 	return render_template('index.html')
 
+@app.route("/count/")
+def count():
+	tuples = connect.getTuples()
+	return render_template('count.html', tuples = tuples)
+
 @app.route("/compare/")
 def compare():
 	return render_template('compare.html')
+
+@app.route("/insights/")
+def insights():
+	return render_template('insights.html')
+
+@app.route("/trends/")
+def trends():
+	return render_template('trends.html')
 
 if __name__ == '__main__':
 	app.run()
--- connect.py
+++ connect.py
@@ -2,11 +2,21 @@ import cx_Oracle
 from pprint import pprint
 from app import app
 
-# connector = 'rwanare/'+app.config['PASSWORD']+'@oracle.cise.ufl.edu:1521/orcl'
-# con = cx_Oracle.connect(connector)
-# cur = con.cursor()
-# cur.execute("SELECT * FROM police_district")
-# for x in cur:
-#     print (x)
+def connecting():
+    connector = 'rwanare/'+app.config['PASSWORD']+'@oracle.cise.ufl.edu:1521/orcl'
+    con = cx_Oracle.connect(connector)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM police_district")
+    for x in cur:
+        print (x)
+    con.close()
 
-# con.close()
+def getTuples():
+    connector = 'rwanare/'+app.config['PASSWORD']+'@oracle.cise.ufl.edu:1521/orcl'
+    con = cx_Oracle.connect(connector)
+    cur = con.cursor()
+    cur.execute("SELECT COUNT(*) FROM crime")
+    for x in cur:
+        return (x[0])
+        break
+    con.close()
